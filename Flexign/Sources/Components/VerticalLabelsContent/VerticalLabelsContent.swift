43251c00def438e04e84fa362784fd2c0abf4fe5
Tomasz <t-bartkowski@american-systems.pl>
2021-04-30 16:02:42 +0200

    buttonSmall in ButtonTypography. VerticallLabelsContent refactor.

--- Flexign/Sources/Components/VerticalLabelsContent/VerticalLabelsContent.swift
+++ Flexign/Sources/Components/VerticalLabelsContent/VerticalLabelsContent.swift
@@ -5,21 +5,18 @@
 //  Created by Tomasz Bartkowski on 29/04/2021.
 //
 
-public class VerticalLabelsContent: UIStackView {
+public class VerticalLabelsContent: UIView {
     // MARK: Lifecycle
 
     public init(style: VerticalLabelsContentStyle? = nil) {
         self.style = style
         super.init(frame: .zero)
 
-        axis = .vertical
-        alignment = .top
-        distribution = .equalSpacing
         layoutView()
         apply(style: style)
     }
 
-    public required init(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
 
@@ -32,14 +29,42 @@ public class VerticalLabelsContent: UIStackView {
 
     open func layoutView() {
         translatesAutoresizingMaskIntoConstraints = false
-        addArrangedSubview(topLabel)
-        addArrangedSubview(bottomLabel)
+        topLabel.translatesAutoresizingMaskIntoConstraints = false
+        bottomLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        topLabel.setContentCompressionResistancePriority(UILayoutPriority(999), for: .vertical)
+        bottomLabel.setContentCompressionResistancePriority(UILayoutPriority(998), for: .vertical)
+
+        topLabel.setContentCompressionResistancePriority(UILayoutPriority(249), for: .horizontal)
+        bottomLabel.setContentCompressionResistancePriority(UILayoutPriority(248), for: .horizontal)
+
+        topLabel.setContentHuggingPriority(UILayoutPriority(249), for: .horizontal)
+        bottomLabel.setContentHuggingPriority(UILayoutPriority(248), for: .horizontal)
+
+        addSubview(topLabel)
+        addSubview(bottomLabel)
+
+        topLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        topLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: topLabel.trailingAnchor).isActive = true
+
+        spacing = bottomLabel.topAnchor.constraint(equalTo: topLabel.bottomAnchor)
+        spacing?.isActive = true
+
+        bottomLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: bottomLabel.bottomAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: bottomLabel.trailingAnchor).isActive = true
     }
 
     open func apply(style: VerticalLabelsContentStyle?) {
         apply(style: style?.viewStyle)
         topLabel.apply(style: style?.topLabelStyle)
         bottomLabel.apply(style: style?.bottomLabelStyle)
-        spacing = style?.spacing ?? Padding.p1
+        spacing?.constant = (style?.spacing ?? Padding.p1)
+        layoutIfNeeded()
     }
+
+    // MARK: Private
+
+    private var spacing: NSLayoutConstraint?
 }
--- Flexign/Sources/Foundation/Typography/ButtonTypography.swift
+++ Flexign/Sources/Foundation/Typography/ButtonTypography.swift
@@ -7,12 +7,15 @@
 
 public enum ButtonTypography: TypeRepresentable {
     case button
+    case buttonSmall
     case buttonText
 
     public var font: UIFont {
         switch self {
         case .button:
             return UIFont.preferredFont(for: .callout, weight: .semibold)
+        case .buttonSmall:
+            return UIFont.preferredFont(for: .subheadline, weight: .semibold)
         case .buttonText:
             return UIFont.preferredFont(for: .callout, weight: .regular)
         }
